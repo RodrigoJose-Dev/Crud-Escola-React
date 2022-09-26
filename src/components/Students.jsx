bb4424139e369319830da80fb3409d31651fc854
Rodrigo José <74773266+RodrigoJose-Dev@users.noreply.github.com>
2022-09-26 17:01:54 -0300

    Tabela finalizada, puxando dados da api

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Table } from "react-bootstrap";
 
 const Students = () => {
-    return <div>Students</div>;
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/students")
+            .then((resp) => resp.json())
+            .then((date) => {
+                setStudents(date);
+            });
+    }, []);
+
+    return (
+        <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Email</th>
+                    <th>Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                {students.map((student) => {
+                    return (
+                        <tr key={student.id}>
+                            <td>{student.name}</td>
+                            <td>{student.email}</td>
+                            <td>editar</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </Table>
+    );
 };
 
 export default Students;
