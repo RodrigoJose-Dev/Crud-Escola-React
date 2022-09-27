b8fc3c7693baeac4a0fff91229c79c828382fe59
Rodrigo José <74773266+RodrigoJose-Dev@users.noreply.github.com>
2022-09-27 13:10:03 -0300

    Delete do aluno concluido

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,16 +1,35 @@
 import { useState, useEffect } from "react";
-import { Table } from "react-bootstrap";
+import { Table, Button } from "react-bootstrap";
 
 const Students = () => {
     const [students, setStudents] = useState([]);
 
     useEffect(() => {
+        searchStudent();
+    }, []);
+
+    const searchStudent = () => {
         fetch("http://localhost:3000/students")
             .then((resp) => resp.json())
             .then((date) => {
                 setStudents(date);
             });
-    }, []);
+    };
+
+    const deleteStudent = (id) => {
+        fetch(`http://localhost:3000/students/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((resp) => resp.json)
+            .then(() => {
+                setStudents(students.filter((student) => student.id !== id));
+                searchStudent();
+            })
+            .catch((err) => console.log(err));
+    };
 
     return (
         <Table striped bordered hover variant="dark">
@@ -27,7 +46,17 @@ const Students = () => {
                         <tr key={student.id}>
                             <td>{student.name}</td>
                             <td>{student.email}</td>
-                            <td>editar</td>
+                            <td>
+                                Atualizar{" "}
+                                <Button
+                                    variant="danger"
+                                    onClick={() => {
+                                        deleteStudent(student.id);
+                                    }}
+                                >
+                                    Excluir
+                                </Button>
+                            </td>
                         </tr>
                     );
                 })}
